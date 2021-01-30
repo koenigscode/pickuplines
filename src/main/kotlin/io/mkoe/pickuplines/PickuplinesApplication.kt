6a3cc33c8c09f096af585f35f2aebf66f2f4725f
Alexander Zach <alexander.zach1@gmail.com>
2021-01-30 17:56:51 +0100

    initial commit

---
+++ src/main/kotlin/io/mkoe/pickuplines/PickuplinesApplication.kt
@@ -0,0 +1,11 @@
+package io.mkoe.pickuplines
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class PickuplinesApplication
+
+fun main(args: Array<String>) {
+	runApplication<PickuplinesApplication>(*args)
+}
---
+++ src/test/kotlin/io/mkoe/pickuplines/PickuplinesApplicationTests.kt
@@ -0,0 +1,13 @@
+package io.mkoe.pickuplines
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class PickuplinesApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
